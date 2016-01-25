99c262e148306b997444dfdf2ddb2225f0fe6850
Arash Saidi <garash84@gmail.com>
2016-01-25 16:47:46 +0100

    fixed header

--- interaction/interaction.js
+++ interaction/interaction.js
@@ -1,13 +1,29 @@
+var bannerDown = true;
+var minHeight = "100px";
+var maxHeight = "600px";
+var animationSpeed = 500;
+
 var animate = function (height) {
 	// animation banner
 	$("#header").animate({
 		"height": height
-	}, 1000, function () {
-		temp.resizeCanvas($("#header").width(), height.replace("px", ""));
+	}, animationSpeed, function () {
+		if (bannerDown) {
+			temp.resizeCanvas($("#header").width(), height.replace("px", ""));
+			bannerDown = false;
+		}
 	});
+
+	if (!bannerDown) {
+		temp.resizeCanvas($("#header").width(), height.replace("px", ""));
+		setTimeout(function () {
+			bannerDown = true;
+		}, animationSpeed + 100);
+	}
+
 }
 
-var reset = function (height, which) {
+var reset = function (which) {
 	// color for banners
 	$(".active").removeClass("active");
 	$(which).addClass("active");
@@ -15,32 +31,50 @@ var reset = function (height, which) {
 
 $(document).ready(function () {
 	$(".design").click(function (event) {
-		reset("100px", ".design");
-		$("#main").load("views/design.html")
+		reset(".design");
+		$("#main").load("views/design.html");
+		if (bannerDown) {
+			animate(minHeight);
+		}
 	});
 
 	$(".video").click(function (event) {
-		reset("100px", ".video");
-		$("#main").load("views/video.html")
+		reset(".video");
+		$("#main").load("views/video.html");
+		if (bannerDown) {
+			animate(minHeight);
+		}
 	});
 
 	$(".music").click(function (event) {
-		reset("100px", ".music");
-		$("#main").load("views/music.html")
+		reset(".music");
+		$("#main").load("views/music.html");
+		if (bannerDown) {
+			animate(minHeight);
+		}
 	});
 
 	$(".text").click(function (event) {
-		reset("100px", ".text");
-		$("#main").load("views/text.html")
+		reset(".text");
+		$("#main").load("views/text.html");
+		if (bannerDown) {
+			animate(minHeight);
+		}
 	});
 
 	$(".about").click(function (event) {
-		reset("100px", ".about");
-		$("#main").load("views/about.html")
+		reset(".about");
+		$("#main").load("views/about.html");
+		if (bannerDown) {
+			animate(minHeight);
+		}
 	});
 
 	$("#header").click(function (event) {
-		reset("600px");
+		reset("");
 		$("#main").html("")
+		if (!bannerDown) {
+			animate(maxHeight);
+		}
 	})
 });
