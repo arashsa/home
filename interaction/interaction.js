827f3a507085741270621e96a295af005ee8e553
Arash Saidi <garash84@gmail.com>
2016-01-25 14:11:59 +0100

    simple routing

--- interaction/interaction.js
+++ interaction/interaction.js
@@ -1,4 +1,8 @@
+var bannerDown = true;
+
 var reset = function (height, which) {
+
+	// animation banner
 	$("#header").animate({
 		"height": height
 	}, 1000, function () {
@@ -7,6 +11,8 @@ var reset = function (height, which) {
 		});
 	});
 	temp.resizeCanvas($("#header").width(), height.replace("px", ""));
+
+	// color for banners
 	$(".active").removeClass("active");
 	$(which).addClass("active");
 };
@@ -14,22 +20,27 @@ var reset = function (height, which) {
 $(document).ready(function () {
 	$(".design").click(function (event) {
 		reset("100px", ".design");
+		$("#main").load("views/design.html")
 	});
 
 	$(".video").click(function (event) {
 		reset("100px", ".video");
+		$("#main").load("views/video.html")
 	});
 
 	$(".music").click(function (event) {
 		reset("100px", ".music");
+		$("#main").load("views/music.html")
 	});
 
 	$(".text").click(function (event) {
 		reset("100px", ".text");
+		$("#main").load("views/text.html")
 	});
 
 	$(".about").click(function (event) {
 		reset("100px", ".about");
+		$("#main").load("views/about.html")
 	});
 
 	$("#header").click(function (event) {
