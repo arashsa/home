5f8eff1a38e6ec934d4541f5a3e93b2684a22aa6
Arash Saidi <garash84@gmail.com>
2016-02-19 00:28:59 +0100

    edits

--- sketches/vintage.js
+++ sketches/vintage.js
@@ -1,26 +1,26 @@
-function x1(s, t) {
-	return s.sin(t / 5) * 150 + s.sin(t / 5) * 200;
+function x1(s, t, y) {
+	return s.sin(t / 5) + s.sin(t / 5) * y * 10;
 }
 
 function y1(s, t) {
-	return s.cos(t / 12) * 200 + s.cos(t / 12) * 30;
+	return s.cos(t / 12) + s.cos(t / 12) * 10;
 }
 
-function x2(s, t) {
-	return s.sin(t / 20) * 200;
+function x2(s, t, y) {
+	return s.sin(t / 20) * s.cos(t / 10) * y;
 }
 
 function y2(s, t) {
-	return s.sin(t / 15) * 200;
+	return s.sin(t / 10) * 100;
 }
 
 var vintage = function (s) {
 	var t = 0;
-	const LINES = 20;
+	const LINES = 30;
 
 	// color
 	var a = 0;
-	var aa = 0.2;
+	var aa = 2;
 	var b = 100;
 	var bb = 1.5;
 	var c = 130;
@@ -30,15 +30,18 @@ var vintage = function (s) {
 		s.createCanvas(s.windowWidth - 15, s.windowHeight - 15);
 		s.translate(s.width / 2, s.height / 2);
 		s.background(50);
-		s.strokeWeight(5);
+		s.strokeWeight(3);
 	}
 
 	s.draw = function () {
-		s.background(50);
-		for (var i = 0; i < LINES; i++) {
-			//			s.stroke(s.random(255));
+		s.background(b, c, a);
+		for (var i = 0; i < LINES * 2; i += 2) {
+			s.fill(c, b, a);
+			s.stroke(c, b, a);
+			s.ellipse(x1(s, t + i, i), x2(s, t + i, i * 3), 10, 10);
+			s.ellipse(y1(s, t + i), y2(s, t + i), 10, 10);
 			s.stroke(a, b, c);
-			s.line(x1(s, t + i), x2(s, t + i), y1(s, t + i), y2(s, t + i));
+			s.line(x1(s, t + i, i), x2(s, t + i, i * 3), y1(s, t + i), y2(s, t + i));
 		}
 		a += aa;
 		b += bb;
